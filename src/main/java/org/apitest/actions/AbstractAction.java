17228ceb356435c3d83f05bb2a0fae2d4e3243f0
Akash Shinde <vertisinfotechakashs@ooyala.com>
2016-06-02 14:41:08 +0530

    added base components

---
+++ src/main/java/org/apitest/actions/AbstractAction.java
@@ -0,0 +1,11 @@
+package org.apitest.actions;
+
+/**
+ * Action defines particular request
+ *
+ * Eg. We want make GET
+ *
+ */
+public class AbstractAction {
+
+}
---
+++ src/main/java/org/apitest/response_middleware/AbstractResponseMiddleware.java
@@ -0,0 +1,7 @@
+package org.apitest.response_middleware;
+
+/**
+ * Created by akash on 02/06/16.
+ */
+public class AbstractResponseMiddleware {
+}
